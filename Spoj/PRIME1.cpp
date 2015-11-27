520e910ba4c470966fe268851c4a90ffa21478fc
rusrushal13 <rusrushal13@gmail.com>
2015-11-27 14:12:33 +0530

    New code

---
+++ Spoj/PRIME1.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+bool isPrime(long long int n)
+{	long long int i=0;
+    	if (n <= 3)
+    	{	return n > 1;
+    	}
+    	else if (n % 2 == 0 || n % 3 == 0)
+    	{	return false;
+    	}
+    	else
+    	{	for (i = 5; i * i <= n; i += 6)
+    		{	if (n % i == 0 || n % (i + 2) == 0)
+    			{	return false;
+            		}
+        	}
+        	return true;
+    	}
+}
+int main()
+{	long long int t,i,k,n,m;
+	cin>>t;
+	for(k=0;k<t;k++)
+	{	cin>>n>>m;
+		for(i=n;i<=m;i++)
+		{	if(isPrime(i))
+				cout<<i<<endl;
+    		}
+	}
+return 0;
+}
